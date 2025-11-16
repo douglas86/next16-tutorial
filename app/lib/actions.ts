fef09bbc63fc9f1949ea1535ac4e16d970147562
douglas <douglasmaxton@gmail.com>
2025-11-16 11:00:37 +0000

    feat: chapter 12 completed

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -28,10 +28,17 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
 
-  await sql`
+  try {
+    await sql`
 INSERT INTO invoices (customer_id, amount, status, date)
 VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
 `;
+  } catch (error) {
+    console.error(error);
+    return {
+      message: "Database Error: failed to create invoice",
+    };
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
@@ -46,10 +53,18 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100;
 
-  await sql`
+  try {
+    await sql`
 UPDATE invoices
 SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
 WHERE ID = ${id}`;
+  } catch (error) {
+    // We'll also log the error the console for now
+    console.error(error);
+    return {
+      message: "Database Error: failed to update invoice",
+    };
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
